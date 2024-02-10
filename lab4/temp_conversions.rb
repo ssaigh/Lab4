67d827c50c9ff8cf8f2cdabdef0dc53cce2f13cf
Salman Al-Saigh <ssaigh@andrew.cmu.edu>
2024-02-10 11:20:03 +0300

    added conversion two way

--- lab4/temp_conversions.rb
+++ lab4/temp_conversions.rb
@@ -1,10 +1,15 @@
-def convert(temp)
+def convert(temp, measure ="F")
     return "Temperature must be an integer" unless temp.class == Integer
     return "Temperature below Absolute Zero" if temp < -474 
-    a = 5.0/9.0
-    b = temp-32.0
-    c = a*b
-    return c
+    if measure == "F"
+        a = 5.0/9.0
+        b = temp-32.0
+        c = a*b
+        return c
+    else
+        d = temp*2 +30.0
+        return d
+    end
 end
 
 
@@ -13,3 +18,7 @@ puts convert(50)
 puts convert(212)
 puts convert("zero")
 puts convert(-500)
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-280, "C")
