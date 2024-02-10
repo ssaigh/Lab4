66b6b397dcf765746a6c76c3f1d83a62d789b9f0
Salman Al-Saigh <ssaigh@andrew.cmu.edu>
2024-02-10 10:39:44 +0300

    Added the second return

--- lab4/temp_conversions.rb
+++ lab4/temp_conversions.rb
@@ -1,5 +1,6 @@
 def convert(temp)
     return "Temperature must be an integer" unless temp.class == Integer
+    return "Temperature below Absolute Zero" if temp < -474 
     a = 5.0/9.0
     b = temp-32.0
     c = a*b
@@ -11,3 +12,4 @@ puts convert(32)
 puts convert(50)          
 puts convert(212)
 puts convert("zero")
+puts convert(-500)
